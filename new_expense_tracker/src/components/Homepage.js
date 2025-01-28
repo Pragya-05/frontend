3c9ee5f5d064253ce18507d4dee229493e597fe4
Pragya-05 <pragyapal2005@gmail.com>
2025-01-28 08:46:10 -0800

    Home page for expanse tracker

---
+++ new_expense_tracker/src/components/Homepage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import img from '../asset/img/dashboard.jpg'
+
+function Homepage() {
+  return (
+    <div className='container'>
+      
+     <div className='leftcontainer'> 
+        <p> </p>
+        <img src={img} alt='Dashboard'/>
+     </div>
+     <div className='rightcontainer'> 
+        <p> </p>
+       
+     </div>
+    </div>
+  )
+}
+
+export default Homepage
